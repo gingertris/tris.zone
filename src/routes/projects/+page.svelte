eb05047ccf0b61570878ac2343a6aa3fe025f4dd
Tris <gingertris1@gmail.com>
2023-02-17 23:00:17 +0000

    content

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -2,10 +2,12 @@
     <div>
         <h2>tris' field</h2>
         <p>a competitive rocket league team matchmaking discord server </p>
+        <i>soon...</i>
     </div>
 
     <div>
         <h2>private 6 mans</h2>
-        <p>a custom matckmaking and lobby creation discord bot, designed for rocket league communities.</p>
+        <p>a custom matckmaking and lobby creation discord bot, designed for rocket league communities</p>
+        <p><i>26/12/2018 - 25/08/2022</i></p>
     </div>
 </div>
