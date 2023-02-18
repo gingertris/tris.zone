e0ad53bfca26223adf5c81e2fb110bfc5420f7db
Tris <gingertris1@gmail.com>
2023-02-18 13:09:01 +0000

    tris.zone title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,12 @@
 
 <script lang="ts">
+	import { transition_in } from "svelte/internal";
     import "../app.sass"
 </script>
+
+<svelte:head>
+    <title>tris.zone</title>
+</svelte:head>
 
 <div id="header">
     <p><a href="/" class="text-pink">tris.zone</a></p>
